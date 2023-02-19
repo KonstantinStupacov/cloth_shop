4d543dfe891cffdfce53df87faee56d22fb8c35f
KonstantinStupacov <kosta55921@gmail.com>
2023-02-19 20:49:34 +0500

    refactor(master): Refactoring

--- config/settings.py
+++ config/settings.py
@@ -38,7 +38,7 @@ INSTALLED_APPS = [
     'django.contrib.messages',
     'django.contrib.staticfiles',
     'rest_framework',
-    'logic_shop'
+    'toy_shop'
 ]
 
 MIDDLEWARE = [
--- toy_shop/apps.py
+++ toy_shop/apps.py
@@ -3,4 +3,4 @@ from django.apps import AppConfig
 
 class LogicShopConfig(AppConfig):
     default_auto_field = 'django.db.models.BigAutoField'
-    name = 'logic_shop'
+    name = 'toy_shop'
